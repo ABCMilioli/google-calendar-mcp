c58bfb19326e74cb40bc43cdc8286bc00e8abb52
ABCMilioli <automacaodebaixocusto@gmail.com>
2025-03-25 14:16:40 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Download do script
+curl -fsSL https://raw.githubusercontent.com/ABCMilioli/google-calendar-mcp/master/setup.sh -o setup.sh
+chmod +x setup.sh
+
+# Executa o script
+./setup.sh 
